ab1dd583000f4a430bff7b0f3ea8dab88e1fe8e5
Aditya Kumbhar <adhkumbhar@gmail.com>
2025-07-19 16:51:17 +0000

    check prime

--- Basic_maths/check_prime.py
+++ Basic_maths/check_prime.py
@@ -1,9 +1,11 @@
 class prime:
     def check(self,x:int)->bool:
-        flag=True
         if x==1:
-            print
+            print("Its coprime")
         for i in range(2,(x//2)+1):
             if x%i==0:
                 return False
-        
+        return True
+
+n=int(input())
+print(prime().check(n))
