a6f94aaf0cca365a9db410b328b37c079b808a2e
Aditya Kumbhar <adhkumbhar@gmail.com>
2025-07-19 16:47:47 +0000

    improved divisor with time complexity reduced

--- Basic_maths/check_prime.py
+++ Basic_maths/check_prime.py
@@ -6,3 +6,4 @@ class prime:
         for i in range(2,(x//2)+1):
             if x%i==0:
                 return False
+        
--- Basic_maths/divisor.py
+++ Basic_maths/divisor.py
@@ -1,10 +1,18 @@
+from math import sqrt
 class factors:
     def all_factors(self,x:int):
         for i in range(1,(x//2)+1):
             if x%i==0:
                 print(i)
         print(x)
+        
+    def impr_all_factors(self,x:int):
+        for i in range(1,int(sqrt(x))+1):
+            if x%i==0:
+                print(i)
+                if (x//i)!=i:
+                    print(x//i)
                 
             
 n=int(input())
-factors().all_factors(n)
+factors().impr_all_factors(n)
